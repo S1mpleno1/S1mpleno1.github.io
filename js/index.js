ce5272561eea004a278393f75e66ed18aeef2b67
S1mpleno1 <92136465+S1mpleno1@users.noreply.github.com>
2022-04-02 19:20:48 +0700

    upload

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/6Zr5LqMAATM?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/xypzmu5mMPY?start=8&autoplay=1&loop=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
